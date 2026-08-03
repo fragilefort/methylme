ab2075b26957ee586368e0559764dbe191f39940
fragilefort <imaddex3759@gmail.com>
2026-08-03 14:49:51 +0200

    setting options individdually

--- rnb_beads/run_rnbbeads.r
+++ rnb_beads/run_rnbbeads.r
@@ -5,19 +5,16 @@ report.dir <- "/vol/COMPEPIWS/groups/wgbs2/methylme/rnb_beads/rnbeads_reports"
 sample.sheet <- "/vol/COMPEPIWS/groups/wgbs2/methylme/rnb_beads/sample_annotation.csv"
 
 dir.create(report.dir, recursive = TRUE, showWarnings = FALSE)
-
 rnb.initialize.reports(report.dir)
 
-rnb.options(
-    assembly = "mm10",
-    import.bed.style = "bismarkCov",
-    differential.comparison.columns = "tissue",
-    differential.report.sites = FALSE,
-    filtering.coverage.threshold = 10,
-    filtering.low.coverage.masking = TRUE,
-    filtering.high.coverage.outliers = TRUE,
-    filtering.missing.value.quantile = 1,
-)
+rnb.options("assembly" = "mm10")
+rnb.options("import.bed.style" = "bismarkCov")
+rnb.options("differential.comparison.columns" = "tissue")
+rnb.options("differential.report.sites" = FALSE)
+rnb.options("filtering.coverage.threshold" = 10)
+rnb.options("filtering.low.coverage.masking" = TRUE)
+rnb.options("filtering.high.coverage.outliers" = TRUE)
+rnb.options("filtering.missing.value.quantile" = 1)
 
 rnb.run.analysis(
     dir.reports = report.dir,
