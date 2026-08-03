3cdc01593e278aa770a6a795463287321cdf84ba
fragilefort <imaddex3759@gmail.com>
2026-08-03 15:22:20 +0200

    changing the sample annotation and r script

--- rnb_beads/run_rnbbeads.r
+++ rnb_beads/run_rnbbeads.r
@@ -4,25 +4,30 @@ data.dir <- "/vol/COMPEPIWS/groups/wgbs2/methylme/CASINO/methylation_coverage"
 report.dir <- "/vol/COMPEPIWS/groups/wgbs2/methylme/rnb_beads/rnbeads_reports"
 sample.sheet <- "/vol/COMPEPIWS/groups/wgbs2/methylme/rnb_beads/sample_annotation.csv"
 
-id.column <- "sampleId"
+if (dir.exists(report.dir)) {
+    unlink(report.dir, recursive = TRUE, force = TRUE)
+}
 
 rnb.options(
     assembly = "mm10",
-    identifiers.column = id.column,
-    import.default.data.type = "bs.bed.dir",
     import.bed.style = "bismarkCov",
+    import.bed.header = FALSE,
     differential.comparison.columns = "tissue",
     differential.report.sites = FALSE,
     filtering.coverage.threshold = 10,
     filtering.low.coverage.masking = TRUE,
     filtering.high.coverage.outliers = TRUE,
-    filtering.missing.value.quantile = 0
+    filtering.missing.value.quantile = 0,
+)
+
+ds <- list(
+    dir = data.dir,
+    sample.sheet = sample.sheet
 )
 
 rnb.run.analysis(
     dir.reports = report.dir,
-    sample.sheet = sample.sheet,
-    data.dir = data.dir,
-    data.type = "bs.bed.dir",
+    data.source = ds,
+    data.type = "bed.dir",
     initialize.reports = TRUE
 )
