f4c2e00cbf96aee59a0e57ef29f475124034b50d
fragilefort <imaddex3759@gmail.com>
2026-08-03 15:07:10 +0200

    correct parameters for rn analysis

--- rnb_beads/run_rnbbeads.r
+++ rnb_beads/run_rnbbeads.r
@@ -4,28 +4,25 @@ data.dir <- "/vol/COMPEPIWS/groups/wgbs2/methylme/CASINO/methylation_coverage"
 report.dir <- "/vol/COMPEPIWS/groups/wgbs2/methylme/rnb_beads/rnbeads_reports"
 sample.sheet <- "/vol/COMPEPIWS/groups/wgbs2/methylme/rnb_beads/sample_annotation.csv"
 
-if (!dir.exists(report.dir)) {
-    dir.create(report.dir, recursive = TRUE, showWarnings = FALSE)
-}
-rnb.initialize.reports(report.dir)
+id.column <- "sampleId"
 
-rnb.options("assembly" = "mm10")
-rnb.options("import.bed.style" = "bismarkCov")
-rnb.options("differential.comparison.columns" = "tissue")
-rnb.options("differential.report.sites" = FALSE)
-rnb.options("filtering.coverage.threshold" = 10)
-rnb.options("filtering.low.coverage.masking" = TRUE)
-rnb.options("filtering.high.coverage.outliers" = TRUE)
-rnb.options("filtering.missing.value.quantile" = 1)
-
-ds <- list(
-    csv.file = sample.sheet,
-    bed.dir = data.dir
+rnb.options(
+    assembly = "mm10",
+    identifiers.column = id.column,
+    import.default.data.type = "bs.bed.dir",
+    import.bed.style = "bismarkCov",
+    differential.comparison.columns = "tissue",
+    differential.report.sites = FALSE,
+    filtering.coverage.threshold = 10,
+    filtering.low.coverage.masking = TRUE,
+    filtering.high.coverage.outliers = TRUE,
+    filtering.missing.value.quantile = 0
 )
 
 rnb.run.analysis(
     dir.reports = report.dir,
-    data.source = ds,
+    sample.sheet = sample.sheet,
+    data.dir = data.dir,
     data.type = "bs.bed.dir",
-    initialize.reports = FALSE
+    initialize.reports = TRUE
 )
